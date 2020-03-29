431bf53abb0fbb65315d08fd1d31e4ed25c2efa7
anassell <41235063+anassell@users.noreply.github.com>
2020-03-29 16:11:08 +0100

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -17,7 +17,7 @@ async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
-    print('Created by niloj')
+    print('Created by AnassEll')
 
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)     
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR TOKEN HERE")                
+client.run("NjkzODM3MjIzMTU3NTYzNDQy.XoC6Cw.dxTQ7nVXzVTRFdrCCA5WvOJuHtg")                
